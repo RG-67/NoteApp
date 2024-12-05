8efabaa68031942bfea8e3e639db787e35e0ba27
RG-67 <rittikghoshkolkata@gmail.com>
2024-12-05 23:17:02 +0530

    note list layout finished

--- src/screens/Note.js
+++ src/screens/Note.js
@@ -1,7 +1,11 @@
-import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Dimensions, FlatList, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import Colors from "../styles/colors";
 import CustomHeader from "../components/CustomHeader";
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { noteItems } from "../utility/TestNoteData";
+
+
+const itemWidth = Dimensions.get('window').width;
 
 function Note ({navigation}) {
     return (
@@ -11,8 +15,24 @@ function Note ({navigation}) {
             <TextInput style={styles.searchTextStyle} multiline={false} placeholder="Search here.." placeholderTextColor={Colors.grey} keyboardType='default'/>
             <Icon name="search" size={30} color={Colors.colorPrimaryVariant} style={{marginEnd: 10}}/>
             </View>
+            <FlatList
+            data={noteItems}
+            keyExtractor={(item) => item.id}
+            numColumns={2}
+            columnWrapperStyle={styles.wrapperStyle}
+            renderItem={({item}) => (
+            <View style={styles.mainItemContainer}>
+            <Text style={styles.dateAndTimeStyle}>{item.date}</Text>
+            <Text style={styles.dateAndTimeStyle}>{item.time}</Text>
+            <View style={styles.noteContainer}>
+                <Text style={styles.noteTitleStyle}>{item.title}</Text>
+                <Text style={styles.noteTextStyle}>{item.note}</Text>
+            </View>
+            </View>
+        )}
+            style={styles.noteItemContainer}/>
             <TouchableOpacity style={styles.floatBtnStyle}>
-                <Icon name="add" size={30} color={Colors.white}/*  style={{alignSelf: 'center'}} *//>
+                <Icon name="add" size={30} color={Colors.white}/>
             </TouchableOpacity>
         </View>
     )
@@ -59,6 +79,54 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.3,
         shadowOffset: {width: 0, height: 2},
         shadowRadius: 3
+    },
+    noteItemContainer: {
+        marginTop: 10,
+        marginHorizontal: 5
+    },
+    wrapperStyle: {
+        justifyContent: 'space-between',
+        alignItems: 'stretch'
+    },
+    mainItemContainer: {
+        borderRadius: 10,
+        backgroundColor: Colors.sallow_green,
+        flex: 1,
+        margin: 5,
+        paddingHorizontal: 5,
+        paddingTop: 5,
+        minHeight: 100,
+        maxWidth: itemWidth / 2 - 15
+    },
+    dateAndTimeStyle: {
+        fontWeight: 'bold',
+        color: Colors.white,
+        fontSize: 13,
+        fontFamily: 'sans-serif'
+    },
+    noteContainer: {
+        borderRadius: 10,
+        backgroundColor: Colors.white,
+        flex: 1,
+        padding: 5,
+        width: '100%',
+        height: '100%',
+        marginTop: 5,
+        marginBottom: 5,
+        minHeight: 150
+    },
+    noteTitleStyle: {
+        fontWeight: 'bold',
+        color: Colors.black,
+        fontSize: 14,
+        fontFamily: 'sans-serif',
+        alignSelf: 'center'
+    },
+    noteTextStyle: {
+        fontWeight: 'normal',
+        color: Colors.black,
+        fontSize: 13,
+        fontFamily: 'sans-serif'
     }
 })
 
---
+++ src/utility/TestNoteData.js
@@ -0,0 +1,15 @@
+
+
+
+export const noteItems = [
+    {id: '1', date: '5th Dec, 2024', time: '20:32', title: 'T1', note: 'First note.'},
+    {id: '2', date: '6th Dec, 2024', time: '20:33', title: 'T2', note: 'Second note.'},
+    {id: '3', date: '7th Dec, 2024', time: '20:34', title: 'T3', note: 'Third note.'},
+    {id: '4', date: '8th Dec, 2024', time: '20:35', title: 'T4', note: 'Forth note.'},
+    {id: '5', date: '9th Dec, 2024', time: '20:36', title: 'T5', note: 'Fifth note.'},
+    {id: '6', date: '5th Dec, 2024', time: '20:32', title: 'T1', note: 'First note.'},
+    {id: '7', date: '6th Dec, 2024', time: '20:33', title: 'T2', note: 'Second note.'},
+    {id: '8', date: '7th Dec, 2024', time: '20:34', title: 'T3', note: 'Third note.'},
+    {id: '9', date: '8th Dec, 2024', time: '20:35', title: 'T4', note: 'Forth note.'},
+    {id: '10', date: '9th Dec, 2024', time: '20:36', title: 'T5', note: 'Fifth note.'}
+];
